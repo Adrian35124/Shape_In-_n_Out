1e53cd6b059f03aebe91cd4a6d38bb8ffa09ec5e
adrian35124 <adrian35124@gmail.com>
2020-06-11 16:48:06 -0500

    Still needs some work !

--- app.js
+++ app.js
@@ -1,44 +1,71 @@
- class Square {
-     constructor (sideLength) {
-        this.sideLength = sideLength;
-        let squareDiv = document.createElement("div")
-        squareDiv.classList.add("squareDiv")
-        squareDiv.style.backgroundColor = getRandomColor();
-        squareDiv.style.height = sideLength + "px";
-        squareDiv.style.width = sideLength + "px";
-        document.body.appendChild(squareDiv);
+const shapeContainer = document.getElementById("shapeContainer")
+class Shape {
+     constructor () {
+        this.shape = document.createElement("div")
+        shapeContainer.appendChild(this.shape);
      }
  }
-// class Rectangle extends Shape {
-//     constructor () {
 
-//     }
-// }
-// class Triangle extends Shape {
-//     constructor () {
-
-//     }
-// }
-class Circle{
+ class Square extends Shape {
+     constructor (sideLength) {
+        super();
+        this.sideLength = sideLength;
+        this.shape.classList.add("squareDiv")
+        this.shape.style.backgroundColor = getRandomColor();
+        this.shape.style.height = sideLength + "px";
+        this.shape.style.width = sideLength + "px";
+        this.shape.style.top = `${Math.floor(Math.random() * 600)}px`
+        this.shape.style.left = `${Math.floor(Math.random() * 600)}px`
+     }
+ }
+class Rectangle extends Shape {
+    constructor (height, width) {
+        super();
+        this.height = height;
+        this.width = width;
+        this.shape.classList.add("rectangleDiv")
+        this.shape.style.backgroundColor = getRandomColor();
+        this.shape.style.height = height + "px"
+        this.shape.style.width = width + "px"
+        this.shape.style.top = `${Math.floor(Math.random() * 600)}px`
+        this.shape.style.left = `${Math.floor(Math.random() * 600)}px`
+    }
+}
+class Triangle extends Shape {
+    constructor (height) {
+        super();
+        this.height = height;
+        this.shape.classList.add("triangleDiv")
+        this.shape.style.height = `${this.height}px`
+        this.shape.style.width = `${this.height}px`
+    }
+}
+class Circle extends Shape {
     constructor (radius) {
+        super();
     this.radius = radius;
-    let circleDiv = document.createElement("div");
-    circleDiv.classList.add("circleDiv");
-    circleDiv.style.backgroundColor = getRandomColor();
-    circleDiv.style.height = 2 * radius + "px";
-    circleDiv.style.width = 2 *radius + "px";
-    circleDiv.style.borderRadius = radius + "px";
-    document.body.appendChild(circleDiv);
+    this.shape.classList.add("circleDiv");
+    this.shape.style.backgroundColor = getRandomColor();
+    this.shape.style.height = 2 * radius + "px";
+    this.shape.style.width = 2 *radius + "px";
+    this.shape.style.borderRadius = radius + "px";
+    this.shape.style.top = `${Math.floor(Math.random() * 600)}px`
+    this.shape.style.left = `${Math.floor(Math.random() * 600)}px`
     }
 } 
 let creatingSquare = () => {
-    let sideLength = $("squareLength").val();
+    let sideLength = $(".sideLength").val();
     let square = new Square(sideLength);
 }
 
 let creatingCircle = () => {
     let circleRadius = $(".circleRadius").val();
     let circle = new Circle(circleRadius);
+}
+let creatingRectangle = () => {
+    let rectangleHeight = $(".rectangleHeight").val();
+    let rectangleWidth = $(".rectangleWidth").val();
+    new Rectangle(rectangleHeight, rectangleWidth)
 }
 
 function getRandomColor() {
